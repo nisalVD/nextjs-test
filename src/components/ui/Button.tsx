c7305a652e3d3e1f378f91635523892d8e6e306f
Nisal Don <nisalvd@gmail.com>
2023-08-13 20:25:45 +1000

    add a reusable button, inspired from shadcn and add intellisence support for it

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
+
+const buttonVariants = cva(
+  "rounded-md shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 text-sm font-semibold",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-blue-600 hover:bg-blue-500 focus-visible:outline-blue-600 text-white",
+      },
+      size: {
+        default: "px-3.5 py-2.5",
+        xs: "px-2 py-1 text-xs",
+        sm: "px-2.5 py-1.5",
+        md: "px-3 py-2",
+        lg: "px-3 py-2",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  isLoading?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, variant, isLoading, size, ...props }, ref) => {
+    return (
+      <button
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
